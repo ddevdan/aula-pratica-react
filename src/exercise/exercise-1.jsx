95ed743e00ad4c1df92d4ba287f29d42c9688c61
= <dnc331@gmail.com>
2022-08-09 00:12:16 -0300

    Add exercise-1.jsx

---
+++ src/exercise/exercise-1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MeuPrimeiroComponenteReact() {
+  /** A função está vazia ://
+   * Você precisa retornar um elemento HTML
+   * para que ela se torne um componente React.
+   */
+}
+
+export default MeuPrimeiroComponenteReact;
---
+++ src/solved/exercise-1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MeuPrimeiroComponenteReact() {
+  return <p>Olá, mundo! Sou um componente React!</p>;
+}
+
+export default MeuPrimeiroComponenteReact;
