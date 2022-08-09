738a0a72c4f526d4b173801e08f3965baeafc5b9
= <dnc331@gmail.com>
2022-08-09 03:42:14 -0300

    Add exercise-7.jsx

---
+++ src/solved/exercise-7.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { CAT_URL, PHOTOS } from '../constants';
+
+function Cat({ cat }) {
+  return (
+    <div>
+      <h3>
+        Futuristic Name:
+        {cat.id}
+      </h3>
+      <img src={cat.url} alt="um gatinho lindo" />
+    </div>
+  );
+}
+
+function ListCats() {
+  const [cats, setCats] = React.useState([]);
+  /**
+   * o useEffect lida com async e mantém a aplicação atualizada
+   * Ele lida com assincronidade e mantém a React atualiazado em relação ao navegador
+   * Recomendação de leitura :) https://bugged.dev/post/The-useEffect-Cheatsheet
+   */
+
+  React.useEffect(() => {
+    fetch(CAT_URL).then((res) => res.json()).then((response) => setCats(response));
+  }, []); // nesse caso o request será feito assim que o componente for montado
+
+  /** Caso a usuária click no botão, um novo request será feito para a API   */
+  const handleRefresh = () => {
+    fetch(CAT_URL).then((res) => res.json()).then((response) => setCats(response));
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Lista de Gatineos</h3>
+        {cats.map((cat) => <Cat cat={cat} key={cat.id} />)}
+      </div>
+
+      <button onClick={handleRefresh}>REFRESH LISTA</button>
+    </div>
+  );
+}
+
+export default ListCats;
