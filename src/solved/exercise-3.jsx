0d69093902e7306ee7e370e883173ac30c3a790f
= <dnc331@gmail.com>
2022-08-09 01:07:08 -0300

    Add exercise-3.jsx

---
+++ src/solved/exercise-3.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ButtonFilho({ cor, handleClick }) {
+  <button style={{ background: cor }} type="button" onClick={handleClick}>
+    {cor}
+  </button>;
+}
+
+function ComponentePai() {
+  // eslint-disable-next-line no-alert
+  const handleClick = () => alert('Você clicou no botão');
+
+  return (
+    <div>
+      <ButtonFilho cor="red" onClick={handleClick} />
+      <ButtonFilho cor="blue" onClick={handleClick} />
+    </div>
+  );
+}
+
+export default ComponentePai;
